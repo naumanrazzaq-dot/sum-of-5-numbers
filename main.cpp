3ef9c8adb914181739536a2aa70c67b5796e5557
Muhammad Nauman <maliknomigep@gmail.com>
2026-03-20 20:13:16 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1,num2,num3,num4,num5,num6;
+	cout<<"enter your English marks: \n";
+	cin>>num1;
+	cout<<"enter your Urdu marks: \n";
+	cin>>num2;
+	cout<<"enter your Computer marks:\n";
+	cin>>num3;
+	cout<<"enter your Calculus marks:  \n";
+	cin>>num3;
+	cout<<"enter your Pf marks:  \n";
+	cin>>num4;
+	cout<<"enter your discrete marks:  \n";
+	cin>>num5;
+	cout<<"enter your isl marks:  \n";
+	cin>>num6;
+	int sum;
+		sum=num1+num2+num3+num4+num5+num6;
+		cout<<sum;
+	return 0;
+}
